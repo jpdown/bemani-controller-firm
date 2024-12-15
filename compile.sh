8aaead7b5c0b5c5f9abb82989774fdaeb7f6caf4
Jaden Down <24631328+jpdown@users.noreply.github.com>
2024-12-15 13:26:01 -0600

    improve pico sdk management

--- compile.sh
+++ compile.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 DO_CLEAN=false
+NUKE_SDK=false
 
-while getopts c flag
+while getopts csn flag
 do
     case "${flag}" in
-        c) DO_CLEAN=true
+        c) 
+            DO_CLEAN=true
+            ;;
+        s) 
+            NUKE_SDK=true
+            ;;
+        n)
+            DO_CLEAN=true
+            NUKE_SDK=true
+            ;;
     esac
 done
 
 if [ $DO_CLEAN = true ]; then
     rm -rf build
+fi
+
+if [ $NUKE_SDK = true ]; then
+    rm -rf pico
 fi
 
 if [ ! -d build ]; then
--- setup-env.sh
+++ setup-env.sh
@@ -2,7 +2,7 @@ mkdir -p build/pico
 
 for REPO in sdk examples playground extras
 do
-	DESTINATION="build/pico/pico-$REPO"
+	DESTINATION="pico/pico-$REPO"
 
 	if [ -d $DESTINATION ]; then
 		echo "already have pico-$REPO"
