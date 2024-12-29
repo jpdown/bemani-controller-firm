541689cfc5617fa5e455f8122005430f4d90083c
Jaden Down <24631328+jpdown@users.noreply.github.com>
2024-12-29 16:14:02 -0600

    new button mapping

--- include/button.hpp
+++ include/button.hpp
@@ -23,8 +23,8 @@ typedef enum {
   NUM_BUTTONS
 } button_t;
 
-static uint8_t BUTTON_TO_PIN_MAP[NUM_BUTTONS] = {2,  4,  6,  8,  10, 12,
-                                                 14, 16, 18, 20, 22};
+static uint8_t BUTTON_TO_PIN_MAP[NUM_BUTTONS] = {2, 3, 4,  5,  6, 7,
+                                                 8, 9, 10, 11, 13};
 
 typedef struct {
   bool pin_state;
